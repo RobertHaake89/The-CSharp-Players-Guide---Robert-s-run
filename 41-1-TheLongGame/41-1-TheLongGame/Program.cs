156c098e9ab855c4b80c598fb5b2464ff998201f
Haake Robert <R.Haake@notraining.intern>
2026-08-18 14:37:56 +0200

    41-1 TheLongGame initial commit

---
+++ 41-1-TheLongGame/41-1-TheLongGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheLongGame;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("\n\t\tTHE LONG GAME\n\n");
+
+        
+    }
+}
