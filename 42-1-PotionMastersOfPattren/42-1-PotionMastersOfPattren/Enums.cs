25beade89b210265c834070b454887409d8f4d6b
Haake Robert <R.Haake@notraining.intern>
2026-08-19 13:51:22 +0200

    42-1 created Potion and Ingredient Enums, minor 41-1 rearrangement

---
+++ 42-1-PotionMastersOfPattren/42-1-PotionMastersOfPattren/Enums.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace PotionMastersOfPattren;
+
+public enum Potion
+{
+    Water = 0,
+    Elixir,
+    Poison,
+    Flying,
+    Invisibility,
+    NightSight,
+    Cloudy,
+    Wraith,
+    Ruined
+}
+
+public enum Ingredient
+{
+    Stardust = 1,
+    Venom,
+    DragonBreath,
+    ShadowGlass,
+    EyeshineGem
+}
---
+++ 42-1-PotionMastersOfPattren/42-1-PotionMastersOfPattren/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PotionMastersOfPattren;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\n\t\tTHE POTION MASTERS OF PATTREN\n\n");
+
+        
+    }
+}
