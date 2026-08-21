898cb31c700a1e61b8a21771b4eed52dfaf95b75
Haake Robert <R.Haake@notraining.intern>
2026-08-21 13:35:02 +0200

    43-1 initial commit

---
+++ 43-1-NavigatingOperandCity/43-1-NavigatingOperandCity/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NavigatingOperandCity;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\n\n\tNavigating Operand City\n\n");
+
+
+    }
+}
