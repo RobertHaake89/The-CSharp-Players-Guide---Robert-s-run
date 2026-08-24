f5fbd2964c3467cac954530ab5f93f3ef6f81228
RobertHaake89 <robert.haake.89@gmail.com>
2026-08-24 14:27:33 +0200

    43-1 Finished, Y-Axis calculation inverted | 42-1 fixed minor grammar bug (vowel)

--- 43-1-NavigatingOperandCity/43-1-NavigatingOperandCity/Coords.cs
+++ 43-1-NavigatingOperandCity/43-1-NavigatingOperandCity/Coords.cs
@@ -1,11 +1,46 @@
 using System;
+using System.Data;
 
 namespace NavigatingOperandCity;
 
-class Corrds
+class Coord
 {
-    public record BlockCoordinate();
+    public enum Direction
+    { 
+        South = 2,
+        West = 4,
+        East = 6,
+        North = 8
+    }
 
-    public record BlockOffset(int RowOffset, int ColumnOffset);
-    public enum Direction { North, East, South, West}
+    public Direction DirectionRow {get; set;}
+    public Direction DirectionColumn {get; set;}
+    public Direction ChosenDirection {get; set;}
+
+    public record BlockCoordinate(float row, float column)
+    {
+        public float Row {get; set;} = row;
+        public float Column {get; set;} =  column;
+        
+        public static BlockCoordinate operator +(BlockCoordinate coord, BlockOffset offCoord)
+        => new BlockCoordinate(coord.Row + offCoord.RowOff,coord.Column - offCoord.ColumnOffset);
+
+        public static BlockCoordinate operator +(BlockCoordinate coord, Direction direction)
+        {
+            return direction switch
+            {
+                Direction.South => new BlockCoordinate(coord.Row, coord.Column + 1),
+                Direction.West => new BlockCoordinate(coord.Row - 1, coord.Column),
+                Direction.East => new BlockCoordinate(coord.Row + 1, coord.Column),
+                Direction.North => new BlockCoordinate(coord.Row, coord.Column - 1),
+                _ => throw new Exception("not valid")
+            };
+        }
+    }
+
+    public record BlockOffset(float RowOffset, float ColumnOffset)
+    {
+        public float RowOff {get; set;} = RowOffset;
+        public float ColumnOff {get; set;} = ColumnOffset;
+    }
 }
--- 43-1-NavigatingOperandCity/43-1-NavigatingOperandCity/Program.cs
+++ 43-1-NavigatingOperandCity/43-1-NavigatingOperandCity/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace NavigatingOperandCity;
 
@@ -9,6 +10,35 @@ class Program
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("\n\n\tNavigating Operand City\n\n");
 
-        
+        while (true)
+        {
+            Console.Clear();
+
+            Console.Write("BlockCoord row: ");
+            float inputCoordRow = Convert.ToInt64(Console.ReadLine());
+            Console.Write("BlockCoord column: ");
+            float inputCoordColumn = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("\nBlockOffset row: ");
+            float inputOffCordRow = Convert.ToInt64(Console.ReadLine());
+            Console.Write("BlockOffset column: ");
+            float inputOffCordColumn = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("\nFor the second Coord add a cardinal direction (Keypad #): ");
+            Coord.Direction inputDirection = (Coord.Direction)Convert.ToInt32(Console.ReadLine());
+
+
+            Coord.BlockCoordinate blockCoord = new(inputCoordRow,inputCoordColumn);
+            Coord.BlockOffset blockOffset = new(inputOffCordRow,inputOffCordColumn);
+
+            Coord.BlockCoordinate resultCoord = blockCoord + blockOffset;
+            Coord.BlockCoordinate resultDirection =  blockCoord + inputDirection;
+
+
+            Console.WriteLine($"\nNew Coord is ({resultCoord.Row}, {resultCoord.Column})");
+            Console.WriteLine($"New Enum-Directed Coord is ({resultDirection.Row}, {resultDirection.Column})");
+
+            Console.ReadKey();
+        }
     }
 }
