b450f6d32980af2d295df485df2dc345b46a8aba
RobertHaake89 <robert.haake.89@gmail.com>
2026-09-03 12:53:29 +0200

    50-1 Finished importing Whitaker nuGet and enabling different colors for loop

---
+++ 50-1-TheRichConsole/50-1-TheRichConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CSharpPlayersGuide.RichConsole;
+
+namespace TheRichConsole;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        for (int i = 100; i >= 0; i--)
+        {
+            if (i > 75) RichConsole.WriteLine($"{i}%", Colors.Green, null);
+            else if (i > 50) RichConsole.WriteLine($"{i}%", Colors.Yellow, null);
+            else if (i > 25) RichConsole.WriteLine($"{i}%", Colors.Orange, null);
+            else RichConsole.WriteLine($"{i}%", Colors.Red, null);
+        }
+    }
+}
